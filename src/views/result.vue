7aee5d0a7ee250bd23a2f7afc8d7027c58ce2aaa
Humanken <3hotpeper@gmail.com>
2024-05-11 19:07:47 +0800

    fix bug: text don't change dynamically

--- src/views/result.vue
+++ src/views/result.vue
@@ -1,81 +1,96 @@
 <template>
-  <div>
-  <!-- 標題欄 (導航欄) -->
-  <Navbar
-      navbar-use-animate-bg="false"
-      brand-text="試算結果"
-      :contents-in-collapse="[
-        { colorClassName: 'link-success', href: '/', text: '返回 首頁' },
-        {
-          colorClassName: 'link-danger',
-          href: 'https://www.tdcc.com.tw/portal/zh/smWeb/qryStock',
-          title: '開啟集保戶股權分散表',
-          text: '查詢 大股東比例',
-          target: '_blank'
-        },
-        {
-          colorClassName: 'link-danger',
-          href: 'https://invest.wessiorfinance.com/stock.html',
-          title: '開啟存股試算',
-          text: '存股 試算',
-          target: '_blank'
-        }
-      ]"
-      :updates-in-collapse="[
-          { id: 'updateDividend', text: '2024-04-xx' },
-          { id: 'updatePrice', text: '22:00' }
-      ]"
-  />
+  <div id="resultContainer">
+    <!-- 標題欄 (導航欄) -->
+    <Navbar
+        navbar-use-animate-bg="false"
+        brand-text="試算結果"
+        :contents-in-collapse="[
+          { colorClassName: 'link-success', href: '/', text: '返回 首頁' },
+          {
+            colorClassName: 'link-danger',
+            href: 'https://www.tdcc.com.tw/portal/zh/smWeb/qryStock',
+            title: '開啟集保戶股權分散表',
+            text: '查詢 大股東比例',
+            target: '_blank'
+          },
+          {
+            colorClassName: 'link-danger',
+            href: 'https://invest.wessiorfinance.com/stock.html',
+            title: '開啟存股試算',
+            text: '存股 試算',
+            target: '_blank'
+          }
+        ]"
+        :updates-in-collapse="[
+            { id: 'updateDividend', text: '2024-04-xx' },
+            { id: 'updatePrice', text: '22:00' }
+        ]"
+    />
 
-  <!-- table 資料 -->
-  <div class="result-table-wrapper">
-    <TableResult :type="tableType" @loading-end="closeLoading" />
-  </div>
+    <!-- table 資料 -->
+    <div class="result-table-wrapper">
+      <TableResult :type="tableType" @loading-end="closeLoading" />
+    </div>
   </div>
 </template>
 
 <script setup>
 
   import Navbar from "@/components/Navbar.vue";
-  import { ref, onMounted } from "vue";
+  import {ref, onMounted, reactive} from "vue";
   import { ElLoading } from "element-plus";
   import TableResult from "@/components/result/ResultTable.vue";
 
   const tableType = ref(history.state.params.tableType)
 
   onMounted(() => {
-    loadingComponent = ElLoading.service({
-      lock: true,
-      fullscreen: true,
-      text: loadingText,
-      background: 'rgba(0, 0, 0, 0.9)'
-    })
-    dynamicLoading()
+    startDynamicLoadingService('resultContainer')
   })
 
   // -------------------------------- Loading ----------------------------------------
-  let loadingComponent = null;
-  const loadingText = ref('試算中');
-  let dynamicTimer = [];
+  const state = reactive({
+    loading: {
+      'component': null,
+      'text': '試算中',
+      'timers': [undefined, undefined]
+    }
+  })
 
-  function dynamicLoading() {
-    const textTimer = setInterval(() => {
-      if (loadingText.value === '試算中...') { loadingText.value = '試算中'; return }
-      loadingText.value += '.'
+  function startDynamicLoadingService(targetID) {
+    state.loading.component = ElLoading.service({
+      target: document.getElementById(targetID),
+      lock: true,
+      fullscreen: true,
+      text: state.loading.text,
+      background: 'rgba(0, 0, 0, 0.9)'
+    })
+    state.loading.timers = [ startTextTimer(), startImgTimer() ]
+  }
+
+  function startTextTimer() {
+    return setInterval(() => {
+      if (state.loading.text === '試算中...') {
+        state.loading.text = '試算中'
+      }
+      else {
+        state.loading.text += '.'
+      }
+      state.loading.component.setText(state.loading.text);
     }, 1000)
+  }
 
-    const imgTimer = setInterval(() => {
+  function startImgTimer() {
+    return setInterval(() => {
       // 添加隨機數，避免瀏覽器緩存
       let gifPath = `/src/assets/image/Loading.gif?${Math.random()}`
-      loadingComponent.$el.style.backgroundImage = `url(${gifPath})`
+      state.loading.component.$el.style.backgroundImage = `url(${gifPath})`
     }, 4000)
-    dynamicTimer = [textTimer, imgTimer]
   }
 
   function closeLoading() {
     const closeTimer = setTimeout(() => {
-      loadingComponent.close();
-      dynamicTimer.forEach(clearInterval);
+      state.loading.component.close();
+      state.loading.timers.forEach(clearInterval);
       clearTimeout(closeTimer)
     }, 4000)
   }
