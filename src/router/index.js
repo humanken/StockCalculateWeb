aa3d7eb0881be73baa939db5f4e10a0a54df0821
Humanken <3hotpeper@gmail.com>
2024-05-28 18:40:42 +0800

    use createWebHistory function

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const  router = createRouter({
-    history: import.meta.env.PROD ? createWebHashHistory(import.meta.env.BASE_URL) : createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/home',
