5cd5448087c5d8f49a438887be72ed7b4c7b990c
humanken <3hotpeper@gmail.com>
2024-06-21 14:09:19 +0000

    2024-06-21 14:09:19

--- src/components/result/index.js
+++
@@ -1,6 +0,0 @@
-// src/components/result/index.js
-
-
-export { default as CardResult } from './CardResult.vue'
-export { default as PaginationResult } from './PaginationResult.vue'
-export { default as TableResult } from './TableResult.vue'
--- src/server/other.js
+++
@@ -1,6 +0,0 @@
-import axios from "@/server/axios.js";
-
-
-export async function getFinalUpdateTime() {
-    return axios.get('/updateTime')
-}
